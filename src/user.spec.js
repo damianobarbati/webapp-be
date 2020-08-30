62b09a236a3467a48b91d9dd0e991f1fa4a4c61f
damiano.barbati <damiano.barbati@gmail.com>
2020-08-30 18:13:17 +0200

    jest

--- src/user.spec.js
+++ src/user.spec.js
@@ -1,11 +1,9 @@
 import { signUp } from './user.js';
 
-describe('', () => {
-    it('', async () => {
-        const result = await signUp({ email: 'john.doe@gmail.com', password: 'p455w0rd' });
+test('sign-up', async () => {
+    const result = await signUp({ email: 'john.doe@gmail.com', password: 'p455w0rd' });
 
-        expect(result).toEqual(jasmine.any(String));
-        expect(result.length).toBeGreaterThanOrEqual(100);
-        expect(result).toMatch(/^.+\..+$/);
-    });
+    expect(result).toEqual(expect.any(String));
+    expect(result.length).toBeGreaterThanOrEqual(100);
+    expect(result).toMatch(/^.+\..+$/);
 });
