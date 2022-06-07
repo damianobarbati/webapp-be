b1329e3e8b2c64776ddf387130873dc0e34393f8
emiliosp <emilio.spatola@gmail.com>
2022-06-07 22:41:33 +0200

    fix router eslint issues

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import { Context, Request } from 'koa';
 import Router from '@koa/router';
 import { HTTP404 } from './errors.js';
 import * as user from './controllers/auth.js';
+import {signIn_fn, signIn_input, signUp_input} from './controllers/auth.js';
 
 export const VERSION_PREFIX = '/v' + process.env.npm_package_version.split('.')[0];
 
@@ -11,9 +12,9 @@ const router = new Router({
 
 router.all('/healthcheck', (ctx: Context) => (ctx.body = true));
 
-router.all('/auth/sign-up', async (ctx: Context) => (ctx.body = await user.signUp(ctx.request.body)));
+router.all('/auth/sign-up', async (ctx: Context) => (ctx.body = await user.signUp(ctx.request.body as signUp_input)));
 
-router.all('/auth/sign-in', (ctx: Context) => (ctx.body = user.signIn(ctx.request.body)));
+router.all('/auth/sign-in', (ctx: Context) => (ctx.body = user.signIn(ctx.request.body as signIn_input)));
 
 router.all('/auth/me', (ctx: Context) => {
   const token = <string>ctx.request.headers['x-token'];
