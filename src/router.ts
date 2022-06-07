f7e64ad58cdfb74c07e4f60377f5b13d264c322a
emiliosp <emilio.spatola@gmail.com>
2022-06-07 22:41:52 +0200

    remove smello

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,8 @@
-import { Context, Request } from 'koa';
+import { Context } from 'koa';
 import Router from '@koa/router';
 import { HTTP404 } from './errors.js';
 import * as user from './controllers/auth.js';
-import {signIn_fn, signIn_input, signUp_input} from './controllers/auth.js';
+import { signIn_input, signUp_input } from './controllers/auth.js';
 
 export const VERSION_PREFIX = '/v' + process.env.npm_package_version.split('.')[0];
 
